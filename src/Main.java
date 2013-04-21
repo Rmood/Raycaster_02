98676f905257bb9fe598893ec531f542a5b88820
tyge felby <tyge_felbt@hotmail.com>
2013-04-21 15:26:49 +0200

    Kerem sux

--- src/Main.java
+++ src/Main.java
@@ -128,6 +128,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Tyge sux!");
+        System.out.println("Kerem sux");
         Main main = new Main();
         main.setup();
         main.args = args;
